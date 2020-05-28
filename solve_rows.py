3b8cd76da9dab91d5b0c7aaca9a2debbe4de4d9c
randy <rclinton@rcanalytics.com>
2020-05-28 07:17:24 -0400

    solves board given some pre-existing data for row-wise constraint

--- solve_rows.py
+++ solve_rows.py
@@ -1,42 +1,55 @@
-from random import choice
+from random import choice, sample
 import numpy as np
 import argparse
+from itertools import permutations
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--board_size", type=int, required=False, default=3)
-    return parser.parse_args()
+# def get_args():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("--board_size", type=int, required=False, default=3)
+#     return parser.parse_args()
 
 
 class Board(object):
-    def __init__(self, board_size) -> None:
-        self.BOARD_SIZE = board_size
-        self.CHOICES = [str(i+1) for i in range(self.BOARD_SIZE)]
-        self.board = np.full((self.BOARD_SIZE, self.BOARD_SIZE), "")
+    def __init__(self, board_size=9) -> None:
+        self.board_size = board_size
+        self.choices = [str(i+1) for i in range(self.board_size)]
+        self.board = np.full((self.board_size, self.board_size), "")
     
     def show(self) -> None:
         print(self.board)
 
-    def choices(self, row) -> str:
+    def chosen_number(self, row) -> str:
         row_set = self.row_as_set(row)
-        return choice( list(set(self.CHOICES) -  row_set) ) 
+        return choice( list(set(self.choices) -  row_set) ) 
         
     def row_as_set(self, row) -> set: 
         return set(row)
 
     def fill_row(self, row_index) -> None:
         for ix, el in enumerate(self.board[row_index]):
-            # print(ix, el)
-            self.board[row_index][ix] = self.choices(self.board[row_index])
+            if el == '':
+                self.board[row_index][ix] = self.chosen_number(self.board[row_index])
+            else:
+                continue
         return
     
+    def make_board_one(self) -> None:
+        self.board[0][0] = '0'
+        self.board[1][1] = '1'
+        self.board[2][2] = '2'
+        return
+
     def solve_board(self) -> None:
-        for i in range(self.BOARD_SIZE):
+        for i in range(self.board_size):
             self.fill_row(i)
 
 if __name__ == "__main__":
-    args = get_args()
-    b  = Board(args.board_size)
+    # args = get_args()
+    b  = Board()
+    b.make_board_one()
+    b.show()
+    print()
+
     b.solve_board()
 
     b.show()
